bbe1885b1a22f05d320a54f0c4f52fa94d4398bb
Administrator <Administrator@BASEPC07.base.srv>
2021-02-26 17:20:20 -0300

    adicionado o insert

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,13 +3,22 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexaoJDBC.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void iniciaBanco() {
 		
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("Douglas");
+		userposjava.setEmail("douglas@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
 		
 	}
 
