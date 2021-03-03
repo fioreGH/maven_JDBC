fbcb177a9c4d790499d6e35595688834bf987483
Administrator <Administrator@BASEPC07.base.srv>
2021-03-03 16:11:20 -0300

    Adicionei as buscas gerais ao B.D e busca por ID. no B.D

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,5 +1,8 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Test;
 
 import conexaoJDBC.SingleConnection;
@@ -8,18 +11,48 @@ import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
-	@Test
+	/*@Test
 	public void iniciaBanco() {
 		
 		UserPosDAO userPosDAO = new UserPosDAO();
 		Userposjava userposjava = new Userposjava();
 		
-		userposjava.setId(6L);
-		userposjava.setNome("Douglas");
-		userposjava.setEmail("douglas@gmail.com");
+		userposjava.setId(7L);
+		userposjava.setNome("Vinicius");
+		userposjava.setEmail("vinicius@gmail.com");
 		
 		userPosDAO.salvar(userposjava);
 		
+	}*/
+	
+	@Test
+	public void iniciaListar() {
+		
+		UserPosDAO userPosDAO = new UserPosDAO();
+		try {
+			List<Userposjava> list = userPosDAO.listar();
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("______________________________________________");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
-
+	
+	@Test
+	public void iniciarBuscar() {
+		
+		UserPosDAO userPosDAO = new UserPosDAO();
+		
+		try {
+			Userposjava userposjava = userPosDAO.buscar(6L);
+			System.out.println(userposjava);
+		} catch (Exception e) {
+		
+			e.printStackTrace();
+		}
+	}
 }
